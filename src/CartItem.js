8c3490b5ac628fe7d61e97cf80f2d1f718cab036
Jaskirat <jaskirathsingh2@gmail.com>
2023-09-02 11:43:53 +0530

    Added constructor and Object de-structor

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,30 @@
 import React from "react";
 class CartItem extends React.Component {
+    constructor (){
+        super()
+        this.state={
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        };
+    };
     render() {
+        const{price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={style.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:"grey"}}>Rs. 999/-</div>
-                    <div style={{color:"grey"}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:"grey"}}>Rs. {price}</div>
+                    <div style={{color:"grey"}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*buttons*/}
+                        <img className="action-icons" alt="increase" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
+                        <img className="action-icons" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+                        <img className="action-icons" alt="delete" src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png" />
                     </div>
                 </div>
             </div>
